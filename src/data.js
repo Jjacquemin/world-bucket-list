a954a747e53b86ae7ffeac3cd388a3cfefeb74b4
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-05-04 08:33:18 +0200

    Ajout de la data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        imagePath: "images/img_maldives.jpg",
+        description: "Maison sur pilotis aux Maldives",
+        done: false,
+        link: "https://www.dreamingofmaldives.com/blog-des-maldives/les-plus-belles-villas-sur-pilotis-que-nous-ayons-vu-aux-maldives-notre-selection-en-photos/",
+        coordinates: {lat: -0.681786, lng: 73.191414}
+    },
+    {
+        id: 2,
+        imagePath: "images/img_kenya.jpg",
+        description: "Faire un Safari au Kenya",
+        done: false,
+        link: "http://www.kenya-guide.com/choix-safari-kenya",
+        coordinates: {lat: -0.52483, lng: 36.664008}
+    },
+    {
+        id: 3,
+        imagePath: "images/img_paris.jpg",
+        description: "Monter sur la Tour Eiffel",
+        done: true,
+        link: "https://www.toureiffel.paris/fr",
+        coordinates: {lat: 48.858227, lng: 2.294559}
+    }    
+]
+
+export {data};
