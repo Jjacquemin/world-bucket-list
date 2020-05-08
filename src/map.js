23a449e53a09772f37b41f9abb06f20a794c8dbd
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-05-08 09:15:38 +0200

    Zoom sur la carte via clic sur marker et retour carte si clic bouton

--- src/map.js
+++ src/map.js
@@ -1,10 +1,17 @@
 let map;
+const resetMapButton = document.querySelector("#reset-map");
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 48.858159, lng: 2.294497},
         zoom: 3,
         mapTypeId: "roadmap" // roadmap, satellite, terrain
     });
+    addMapListeners();
+}
+
+function addMapListeners() {
+    resetMapButton.addEventListener("click", resetMap);
 }
 
 function addMarkerOnMap(dream) {
@@ -13,6 +20,21 @@ function addMarkerOnMap(dream) {
         map : map,
         icon: dream.done ? "images/marker_done.png" : "images/marker.png"
     });
+    
+    marker.addListener('click', function() {zoomOn(marker.getPosition())});
+    
+}
+                       
+function zoomOn(position) {
+        map.setZoom(20);
+        map.setCenter(position);
+        map.setMapTypeId("satellite");
+}
+
+function resetMap() {
+        map.setZoom(3);
+        map.setCenter({lat: 48.858159, lng: 2.294497});
+        map.setMapTypeId("roadmap");
 }
 
 export {initMap,addMarkerOnMap};
